6058d642ba2506579b3a6306e63e672cd658920e
elenapetra <elena_petrasco@epam.com>
2024-04-11 13:33:24 +0300

    Refactor: Moved the /import endpoint into PageProductImport.tsx file

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://w4j3h23n1m.execute-api.eu-west-1.amazonaws.com/dev/import",
+  import: "https://w4j3h23n1m.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://zq3zdypgs7.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
